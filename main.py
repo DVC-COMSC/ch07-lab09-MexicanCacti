6ec55406f78cb5f1766f3b4b6e841b640e690870
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-08 18:14:17 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ names = []
 for i in range(len(name)):
     names.append(name[i])
 
-shortest = name[0]
-longest = name[0]
+shortest = names[i]
+longest = names[i]
 for i in range(len(names)):
     if len(names[i]) <= len(shortest) and names[i] < shortest:
         shortest = names[i]
-    elif len(names[i]) >= len(longest) and names[i] > longest:
+        continue
+    elif len(names[i]) >= len(longest) and names[i] < longest:
         longest = names[i]
 
 print(shortest,longest)
