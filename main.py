663fda7eb13491d8ffd4cc53b0f25bf4680dfdf2
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-08 17:47:22 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 # ******************************
 # Make your Code
 # ******************************
+name = input().split()
+names = []
+
+for i in range(len(name)):
+    names.append(name[i])
+
+shortest = name[0]
+longest = name[0]
+for i in range(len(names)):
+    if names[i] < shortest:
+        shortest = names[i]
+    elif names[i] > longest:
+        longest = names[i]
+
+print(shortest,longest)
